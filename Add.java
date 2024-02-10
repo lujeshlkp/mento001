ecbc2a1bb3e7bbfd2f912d1904fb64a4a63f5d5c
lujeshlkp <lkplujesh@gmail.com>
2024-02-10 20:47:51 +0530

    add java file

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Add{
+ public static void main(String[] args){
+  int f=198;
+  int s=112;
+
+int add = f + s;
+ System.out.println("addition of " + f + " and " + s + " is " + add );
+
+}
+}
